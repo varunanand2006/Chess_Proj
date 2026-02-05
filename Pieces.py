064bd070032eab4730937f4e9807e0ef351af707
Varun Anand <varun.anand2006@gmail.com>
2026-02-04 23:31:07 -0500

    Pieces class

--- Pieces.py
+++ Pieces.py
@@ -1,3 +1,14 @@
+
+
+# General parent class for the pieces classes
+
+
+def is_enemy(piece1, piece2):
+   if piece1 is None or piece2 is None:
+       return False
+   return piece1.color != piece2.color
+
+
 class Piece:
    name = " "
    color = ""
@@ -20,3 +31,230 @@ class Piece:
 
    def __str__(self):
        return " "
+
+
+
+
+# Special pieces for each class
+class Pawn(Piece):
+   def __init__(self, color, x, y):
+       super().__init__("Pawn", color, x, y, 1)
+
+
+   def get_attack_squares(self, board):
+       attacks = []
+       direction = -1 if self.color == "White" else 1
+
+
+       for dx in (-1, 1):
+           x = self.x + dx
+           y = self.y + direction
+           if board.in_bounds(x, y):
+               attacks.append((x, y))
+
+
+       return attacks
+
+
+   def get_pseudo_legal_moves(self, board):
+       moves = []
+       # Defining direction and start row
+       if self.color == "White":
+           direction = -1
+           start_row = 6
+       else:
+           direction = 1
+           start_row = 1
+
+
+       # Forward 1
+       nx, ny = self.x, self.y + direction
+       if board.is_empty(nx, ny):
+           moves.append((nx, ny))
+
+
+           # Forward 2 from start  (pawn's very first move)
+           if self.y == start_row:
+               ny2 = ny + direction
+               if board.is_empty(nx, ny2):
+                   moves.append((nx, ny2))
+
+
+       # Captures
+       for dx in [-1, 1]:
+           cx, cy = self.x + dx, self.y + direction
+           if board.is_enemy_square(self, cx, cy):
+               moves.append((cx, cy))
+
+
+       return moves
+
+
+   def __str__(self):
+       if self.color == "White":
+           return "P"
+       elif self.color == "Black":
+           return "p"
+       else:
+           return " "
+
+
+class Bishop(Piece):
+   def __init__(self, color, x, y):
+       super().__init__("Bishop", color, x, y, 3)
+
+
+   def get_pseudo_legal_moves(self, board):
+       moves = []
+       directions = [(1, 1), (-1, -1), (1, -1), (-1, 1)]
+
+
+       for dx, dy in directions:
+           x, y = self.x, self.y
+           while True:
+               x += dx
+               y += dy
+               if not board.in_bounds(x, y):
+                   break
+               if board.is_empty(x, y):
+                   moves.append((x, y))
+               else:
+                   if board.is_enemy_square(self, x, y):
+                       moves.append((x, y))
+                   break
+       return moves
+
+
+   def __str__(self):
+       if self.color == "White":
+           return "B"
+       elif self.color == "Black":
+           return "b"
+       else:
+           return " "
+
+
+class Knight(Piece):
+   def __init__(self, color, x, y):
+       super().__init__("Knight", color, x, y, 3)
+
+
+   def get_pseudo_legal_moves(self, board):
+       moves = []
+       directions = [(2, 1), (1, 2), (-2, 1), (-1, 2), (-2, -1), (-1, -2), (2, -1), (1, -2)]
+       for dx, dy in directions:
+           x, y = self.x, self.y
+           x += dx
+           y += dy
+           if board.in_bounds(x, y):
+               if board.is_empty(x, y) or board.is_enemy_square(self, x, y):
+                   moves.append((x, y))
+       return moves
+
+
+   def __str__(self):
+       if self.color == "White":
+           return "N"
+       elif self.color == "Black":
+           return "n"
+       else:
+           return " "
+
+
+class Rook(Piece):
+   def __init__(self, color, x, y):
+       super().__init__("Rook", color, x, y, 5)
+
+
+   def get_pseudo_legal_moves(self, board):
+       moves = []
+       directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+
+       for dx, dy in directions:
+           x, y = self.x, self.y
+           while True:
+               x += dx
+               y += dy
+               if not board.in_bounds(x, y):
+                   break
+               if board.is_empty(x, y):
+                   moves.append((x, y))
+               else:
+                   if board.is_enemy_square(self, x, y):
+                       moves.append((x, y))
+                   break
+       return moves
+
+
+   def __str__(self):
+       if self.color == "White":
+           return "R"
+       elif self.color == "Black":
+           return "r"
+       else:
+           return " "
+
+
+class Queen(Piece):
+   def __init__(self, color, x, y):
+       super().__init__("Queen", color, x, y, 9)
+
+
+   def get_pseudo_legal_moves(self, board):
+       moves = []
+       directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, 1), (1, -1), (-1, -1)]
+
+
+       for dx, dy in directions:
+           x, y = self.x, self.y
+           while True:
+               x += dx
+               y += dy
+               if not board.in_bounds(x, y):
+                   break
+               if board.is_empty(x, y):
+                   moves.append((x, y))
+               else:
+                   if board.is_enemy_square(self, x, y):
+                       moves.append((x, y))
+                   break
+       return moves
+
+
+   def __str__(self):
+       if self.color == "White":
+           return "Q"
+       elif self.color == "Black":
+           return "q"
+       else:
+           return " "
+
+
+class King(Piece):
+   def __init__(self, color, x, y):
+       super().__init__("King", color, x, y, 10)
+
+
+   def get_pseudo_legal_moves(self, board):
+       moves = []
+       directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, 1), (1, -1), (-1, -1)]
+
+
+       for dx, dy in directions:
+           x, y = self.x, self.y
+           x += dx
+           y += dy
+           if board.in_bounds(x, y):
+               if board.is_empty(x, y) or board.is_enemy_square(self, x, y):
+                   moves.append((x, y))
+       return moves
+
+
+   def __str__(self):
+       if self.color == "White":
+           return "K"
+       elif self.color == "Black":
+           return "k"
+       else:
+           return " "
